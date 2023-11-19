c932e8d88fe9fa9d88eb3dcb4ff8cdcca179718c
Qamar Ali <qamarali205@gmail.com>
2023-11-19 13:56:52 +0530

    Visa api

---
+++ enums/issuedType.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    NO_VISA: 'NO_VISA',
+    WEEKLY_VISA: 'WEEKLY_VISA',
+    MONTHLY_VISA: 'MONTHLY_VISA',
+    VISA_ON_ARRIVAL:'VISA_ON_ARRIVAL'
+})
--- model/visaModel/visaModel.js
+++ model/visaModel/visaModel.js
@@ -1,40 +1,52 @@
-const mongoose = require('mongoose');
-const status=require ('../../enums/status')
-const mongoosePaginate = require('mongoose-paginate-v2');
-mongoose.pluralize(null)
-const visaSchema = new mongoose.Schema({
-    countryName:{
-        type:String
+const mongoose = require("mongoose");
+const status = require("../../enums/status");
+const mongoosePaginate = require("mongoose-paginate-v2");
+const issuedType = require("../../enums/issuedType");
+mongoose.pluralize(null);
+const visaSchema = new mongoose.Schema(
+  {
+    countryName: {
+      type: String,
     },
-    price:{
-        type:Number
+    price: {
+      type: Number,
     },
-    validityPeriod:{
-        type:String
+    validityPeriod: {
+      type: String,
     },
-    lengthOfStay:{
-        type:String
+    lengthOfStay: {
+      type: String,
     },
-    gallery:[{
-        type:String
-    }],
-    status:{
-        type:String,
-        default:status.ACTIVE
-    },
-    visaType:{
+    gallery: [
+      {
         type: String,
-        // enum:[],
-     },
-     govermentFees:{
-        type:Number
-     },
-     platFormFees:{
-        type:Number
-     },
-    daysToProcess: {
-        type: Number, 
+      },
+    ],
+    status: {
+      type: String,
+      default: status.ACTIVE,
     },
-},{timestamps: true})
+    issuedType: {
+      type: String,
+      enum: [issuedType.MONTHLY_VISA, issuedType.NO_VISA, issuedType.WEEKLY_VISA,issuedType.VISA_ON_ARRIVAL],
+    },
+    visaType: {
+      type: String,
+    },
+    governmentFees: {
+      type: Number,
+    },
+    platFormFees: {
+      type: Number,
+    },
+    daysToProcess: {
+      type: Number,
+    },
+    continent:{
+      type:String
+    }
+  },
+  { timestamps: true }
+);
 visaSchema.plugin(mongoosePaginate);
 module.exports = mongoose.model("visa", visaSchema);

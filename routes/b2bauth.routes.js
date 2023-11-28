31817a48f6a45bc87bc269e977ba86ee32db6055
Qamar Ali <qamarali205@gmail.com>
2023-11-28 17:37:04 +0530

    fixDeparture data

--- routes/b2bauth.routes.js
+++ routes/b2bauth.routes.js
@@ -58,5 +58,6 @@ module.exports = function (app) {
 
 
   app.post("/skyTrails/fixDeparturedata", controller.fixDeparturedata);
+  app.get("/skyTrails/fixDeparturefilter",controller.fixDeparturefilter)
 
 };

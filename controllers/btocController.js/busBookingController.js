3f3d2034d26fd23c96fa4a4929ec42d55149d3eb
node-charu <yadavcharu604@gmail.com>
2023-12-09 13:09:12 +0530

    modify whatsapp message of busbooking

--- controllers/btocController.js/busBookingController.js
+++ controllers/btocController.js/busBookingController.js
@@ -26,7 +26,7 @@ exports.busBooking = async (req, res, next) => {
 
     const result = await createUserBusBooking(data);
     const userName=result.passenger[0].firstName+result.passenger[0].lastName
-    const message = `Hello ${data.name} ,Thank you for booking your hotel stay with TheSkytrails. Your reservation is confirmed! Please click on url to see details:. Or You Can login theskytrails.com/login`
+    const message = `Hello ${userName} ,Thank you for booking your hotel stay with TheSkytrails. Your reservation is confirmed! Please click on url to see details:. Or You Can login theskytrails.com/login`
      await sendSMS.sendSMSBusBooking(result.passenger[0].Phone,userName);  
      await whatsApi.sendWhatsAppMessage(result.passenger[0].Phone,message)
     await commonFunction.BusBookingConfirmationMail(data)
